9e236027cfb6cb33e36a3b889f0911f506bdaa89
wolf <18803547489@163.com>
2021-02-05 18:20:44 +0800

    emdes

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * JavaScript
+ */
+$(document).ready(function () {
+    // $("#test1").animate({bottom:'200px'},1000);
+    // $("#test2").animate({bottom:'200px'},1000);
+    // $(".n1").fadeIn(3000);
+    $("#test").fadeIn(2000);
+});
