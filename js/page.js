d3940154491f853dbd3b997c4b8393aa3ce5d49f
zc55 <zhaoc1154796681@163.com>
2021-02-07 16:45:59 +0800

    7

---
+++ js/page.js
@@ -0,0 +1,12 @@
+/*页面加载完执行*/
+$('#main').load("hade.html");
+/*获取元素并异步加载页面到指定div*/
+$("#contacts").click(function () {
+    $('#main').load("contacts.html");
+});
+$("#contacts2").click(function () {
+    $('#main').load("contacts.html");
+});
+$("#contacts3").click(function () {
+    $('#main').load("contacts.html");
+});
